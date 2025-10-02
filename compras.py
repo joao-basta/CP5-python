79810eb19b289e26212e62f92560be465c5c53c6
Joao Paulo <bpjoao00@gmail.com>
2025-10-02 13:52:40 -0300

    adicionando tratamento de erro e funcionalidades as funcoes

--- compras.py
+++ compras.py
@@ -1,20 +1,47 @@
-def listaProd():   
-    with open("listaProdutos.txt", "r", encoding="utf-8") as arquivoPler: 
-        for linha in arquivoPler:
-            print(linha, end="")
+logado = False
+historico_disponivel = False
 
+def listaProd(): 
+    total = 0.0
+    quantidade = 0 
+    try:
+        with open("listaProdutos.txt", "r", encoding="utf-8") as arquivoPler: 
+            for linha in arquivoPler:
+                partes = linha.strip().split(':')
+                if len(partes) == 2:
+                    nome, preco = partes
+                    try:
+                        preco = float(preco)
+                        total += preco
+                        quantidade += 1
+                        print(f" produto: {nome} | Preco: R$ {preco:.2f}")
+                    except ValueError:
+                        print(f"Erro ao ler o preço do produto: {nome.strip()}")
+
+
+                print(f"\nTotal de produtos: {quantidade}")
+                print(f"Valor total da compra: R$ {total:.2f}")
+    except FileNotFoundError:
+        print("A lista de produtos ainda não foi criada.")
+        return False
+    
 def logar():
+    global logado
     login = input("Insira a sua senha: ")
-    with open("conta.txt", "r", encoding="utf-8") as arquivoCler:
-        for linha in arquivoCler:
-            if "Senha: " in linha:
-                senha = linha[7:]
-
-        if senha.strip() == login:
-            print("deu certo o login")
-        else:
-            print("Não deu certo")
-
+    try:
+        with open("conta.txt", "r", encoding="utf-8") as arquivoCler:
+            for linha in arquivoCler:
+                if "Senha: " in linha:
+                    senha = linha[7:]
+                    if senha.strip() == login:
+                        print("deu certo o login")
+                        logado = True
+                    else:
+                        print("Não deu certo")
+                        return False
+    except FileNotFoundError:
+        print("Arquivo não encontrado. crie uma conta primeiro.")
+        return False
 
 
 
@@ -38,14 +65,3 @@ match opcao:
         listaProd()
     case 2:
         logar()
-    
-        
-
-
-
-
-
-
-
-
-
